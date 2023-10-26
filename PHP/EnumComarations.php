4268d6761ea8863fe6358e3fdd7fd9b2273e6bd5
vanilium <vanilium@users.noreply.github.com>
2023-10-26 17:04:27 +0300

    Create EnumComarations.php
    Common solution of comparation of enums as trait.
    Allowed to compare enums with value as string or enum case

---
+++ PHP/EnumComarations.php
@@ -0,0 +1,21 @@
+<?php
+
+trait EnumComarations
+{
+  public function is($compareWith): bool
+	{
+		if(!enum_exists(self::class)) {
+			throw new \RuntimeException('Unresolved  applying the comparation trait');
+		}
+		
+		if(is_string($compareWith) && null === ($compareWith = self::tryFrom($compareWith))) {
+			return false;
+		}
+		
+		if(enum_exists(get_class($compareWith)) && is_a($compareWith, self::class)) {
+			return $compareWith === $this;
+		}
+		
+		return false;
+	}
+}
